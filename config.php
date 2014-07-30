ca79fafa187f64e4fc85b8bdf0c319cface97be3
sulsull <coolcmp424@yahoo.com>
2014-07-29 22:36:38 -0400

    Wunderground Implementation
    JSON implemented

--- config.php
+++ config.php
@@ -4,5 +4,18 @@ $config = array();
 
 $config['roomate1HWFeed'] = "https://ginseng.netap.pw/onenote/cameron.xml";
 $config['roomate2HWFeed'] = "https://ginseng.netap.pw/onenote/james.xml";
-
+$weatherDataHour  = "http://api.wunderground.com/api/21f13a51c3b0a8b1/hourly/q/MA/Amherst.json";
+$localweatherDataHour = "/Tenjin/wunderground/weatherdataHour.json";
+$weatherDataWeekly = "http://api.wunderground.com/api/21f13a51c3b0a8b1/forecast10day/q/MA/Amherst.json";
+$localweatherDataWeekly = "/Tenjin/wunderground/weatherdataWeekly.json";
+$units = "english"; // english or metric
+		if ($units == "english"){
+			$tempunits = "fahrenheit";
+			$volunits = "in";
+			$speedunits = "mph";
+		} else {
+			$tempunits = "celsius";
+			$volunits = "mm";
+			$speedunits = "kph";
+		}
 ?>
