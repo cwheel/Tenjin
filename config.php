7ebefe43cc1a27f728518b37cab1917dbee2f29c
sulsull <coolcmp424@yahoo.com>
2014-07-31 19:48:00 -0400

    Finalizing Wunderground Modules

--- config.php
+++ config.php
@@ -2,7 +2,4 @@
 
 $config = array();
 
-$config['roomate1HWFeed'] = "https://ginseng.netap.pw/onenote/cameron.xml";
-$config['roomate2HWFeed'] = "https://ginseng.netap.pw/onenote/james.xml";
-
 ?>
--- data_providers/wunderground.php
+++ data_providers/wunderground.php
@@ -6,53 +6,53 @@
 
 		include('../config.php');
 	    $weatherArray = array();
-	    $loop = 0;
+	    $downloadedweatherdata = null;
 
-		if (file_exists($localWeatherData)){
-			if (date("d",filemtime($localWeatherData)) != date("d")){
-				$downloadedweatherdata = downloadWeatherData($weatherDataHour);
-			}else if (date("H",filemtime($localWeatherData)) <= date("H") - 1){
-				$downloadedweatherdata = downloadWeatherData($weatherDataHour);
+	    // Checks if the local File is older than 1 hour
+		if (file_exists($config['localWeatherData'])){
+			if (date("d",filemtime($config['localWeatherData'])) != date("d")){
+				$downloadedweatherdata = downloadWeatherData($config['weatherDataHour']);
+			}else if (date("H",filemtime($config['localWeatherData'])) <= date("H") - 3){
+				$downloadedweatherdata = downloadWeatherData($config['weatherDataHour']);
 			}else{
-				return file_get_contents($localWeatherData);
+				return file_get_contents($config['localWeatherData']);
 			}
 		}else{
-			$downloadedweatherdata = downloadWeatherData($weatherDataHour);
+			$downloadedweatherdata = downloadWeatherData($config['weatherDataHour']);
 		}
 
-		while ($loop < 36){
-			$weatherArray['hourly_forecast'][$loop]['hour'] = $downloadedweatherdata['hourly_forecast'][$loop]['FCTTIME']['hour'];
-			$weatherArray['hourly_forecast'][$loop]['temp'] = $downloadedweatherdata['hourly_forecast'][$loop]['temp'][$units];
-			$weatherArray['hourly_forecast'][$loop]['condition'] = $downloadedweatherdata['hourly_forecast'][$loop]['wx'];
-			$weatherArray['hourly_forecast'][$loop]['icon'] = $downloadedweatherdata['hourly_forecast'][$loop]['icon'];
-			$weatherArray['hourly_forecast'][$loop]['feelsLike'] = $downloadedweatherdata['hourly_forecast'][$loop]['feelslike'][$units];
-			$weatherArray['hourly_forecast'][$loop]['chancePrecipitation'] = $downloadedweatherdata['hourly_forecast'][$loop]['pop'];
-			$weatherArray['hourly_forecast'][$loop]['rainfall'] = $downloadedweatherdata['hourly_forecast'][$loop]['qpf'][$units];
-			$weatherArray['hourly_forecast'][$loop]['snowfall'] = $downloadedweatherdata['hourly_forecast'][$loop]['snow'][$units];
-			$weatherArray['hourly_forecast'][$loop]['windspeed'] = $downloadedweatherdata['hourly_forecast'][$loop]['wspd'][$units];
-			++$loop;
+		for ($i = 0; $i < 36; $i++){
+			$weatherArray['hourly_forecast'][$i]['hour'] = $downloadedweatherdata['hourly_forecast'][$i]['FCTTIME']['hour'];
+			$weatherArray['hourly_forecast'][$i]['temp'] = $downloadedweatherdata['hourly_forecast'][$i]['temp'][$config['units']];
+			$weatherArray['hourly_forecast'][$i]['condition'] = $downloadedweatherdata['hourly_forecast'][$i]['wx'];
+			$weatherArray['hourly_forecast'][$i]['icon'] = $downloadedweatherdata['hourly_forecast'][$i]['icon'];
+			$weatherArray['hourly_forecast'][$i]['feelsLike'] = $downloadedweatherdata['hourly_forecast'][$i]['feelslike'][$config['units']];
+			$weatherArray['hourly_forecast'][$i]['chancePrecipitation'] = $downloadedweatherdata['hourly_forecast'][$i]['pop'];
+			$weatherArray['hourly_forecast'][$i]['rainfall'] = $downloadedweatherdata['hourly_forecast'][$i]['qpf'][$config['units']];
+			$weatherArray['hourly_forecast'][$i]['snowfall'] = $downloadedweatherdata['hourly_forecast'][$i]['snow'][$config['units']];
+			$weatherArray['hourly_forecast'][$i]['windspeed'] = $downloadedweatherdata['hourly_forecast'][$i]['wspd'][$config['units']];
 		}
 
-		$downloadedweatherdata = downloadWeatherData($weatherDataWeekly);
-		$loop = 0;
-		while ($loop < 7){
-			$weatherArray['weekly_forecast'][$loop]['date']['day'] = $downloadedweatherdata['forecast']['simpleforecast']['forecastday'][$loop]['date']['day'];
-			$weatherArray['weekly_forecast'][$loop]['date']['month'] = $downloadedweatherdata['forecast']['simpleforecast']['forecastday'][$loop]['date']['month'];
-			$weatherArray['weekly_forecast'][$loop]['date']['year'] = $downloadedweatherdata['forecast']['simpleforecast']['forecastday'][$loop]['date']['year'];
-			$weatherArray['weekly_forecast'][$loop]['date']['weekday'] = $downloadedweatherdata['forecast']['simpleforecast']['forecastday'][$loop]['date']['weekday'];
-			$weatherArray['weekly_forecast'][$loop]['temperature']['high'] = $downloadedweatherdata['forecast']['simpleforecast']['forecastday'][$loop]['high'][$tempunits];
-			$weatherArray['weekly_forecast'][$loop]['temperature']['low'] = $downloadedweatherdata['forecast']['simpleforecast']['forecastday'][$loop]['low'][$tempunits];
-			$weatherArray['weekly_forecast'][$loop]['chancePrecipitation'] = $downloadedweatherdata['forecast']['simpleforecast']['forecastday'][$loop]['pop'];
-			$weatherArray['weekly_forecast'][$loop]['rainfall'] = $downloadedweatherdata['forecast']['simpleforecast']['forecastday'][$loop]['qpf_day'][$volunits];
-			$weatherArray['weekly_forecast'][$loop]['snowfall'] = $downloadedweatherdata['forecast']['simpleforecast']['forecastday'][$loop]['snow_day'][$volunits];
-			$weatherArray['weekly_forecast'][$loop]['condition'] = $downloadedweatherdata['forecast']['simpleforecast']['forecastday'][$loop]['conditions'];
-			$weatherArray['weekly_forecast'][$loop]['icon'] = $downloadedweatherdata['forecast']['simpleforecast']['forecastday'][$loop]['icon'];
-			$weatherArray['weekly_forecast'][$loop]['windspeed'] = $downloadedweatherdata['forecast']['simpleforecast']['forecastday'][$loop]['avewind'][$speedunits];
-			$weatherArray['weekly_forecast'][$loop]['humidity'] = $downloadedweatherdata['forecast']['simpleforecast']['forecastday'][$loop]['avehumidity'];
-			++$loop;
+		$downloadedweatherdata = downloadWeatherData($config['weatherDataWeekly']);
+
+		for ($i = 0; $i < 7; $i++){
+			$weatherArray['weekly_forecast'][$i]['date']['day'] = $downloadedweatherdata['forecast']['simpleforecast']['forecastday'][$i]['date']['day'];
+			$weatherArray['weekly_forecast'][$i]['date']['month'] = $downloadedweatherdata['forecast']['simpleforecast']['forecastday'][$i]['date']['month'];
+			$weatherArray['weekly_forecast'][$i]['date']['year'] = $downloadedweatherdata['forecast']['simpleforecast']['forecastday'][$i]['date']['year'];
+			$weatherArray['weekly_forecast'][$i]['date']['weekday'] = $downloadedweatherdata['forecast']['simpleforecast']['forecastday'][$i]['date']['weekday'];
+			$weatherArray['weekly_forecast'][$i]['temperature']['high'] = $downloadedweatherdata['forecast']['simpleforecast']['forecastday'][$i]['high'][$config['tempunits']];
+			$weatherArray['weekly_forecast'][$i]['temperature']['low'] = $downloadedweatherdata['forecast']['simpleforecast']['forecastday'][$i]['low'][$config['tempunits']];
+			$weatherArray['weekly_forecast'][$i]['chancePrecipitation'] = $downloadedweatherdata['forecast']['simpleforecast']['forecastday'][$i]['pop'];
+			$weatherArray['weekly_forecast'][$i]['rainfall'] = $downloadedweatherdata['forecast']['simpleforecast']['forecastday'][$i]['qpf_day'][$config['volunits']];
+			$weatherArray['weekly_forecast'][$i]['snowfall'] = $downloadedweatherdata['forecast']['simpleforecast']['forecastday'][$i]['snow_day'][$config['volunits']];
+			$weatherArray['weekly_forecast'][$i]['condition'] = $downloadedweatherdata['forecast']['simpleforecast']['forecastday'][$i]['conditions'];		
+			$weatherArray['weekly_forecast'][$i]['icon'] = $downloadedweatherdata['forecast']['simpleforecast']['forecastday'][$i]['icon'];
+			$weatherArray['weekly_forecast'][$i]['windspeed'] = $downloadedweatherdata['forecast']['simpleforecast']['forecastday'][$i]['avewind'][$config['speedunits']];
+			$weatherArray['weekly_forecast'][$i]['humidity'] = $downloadedweatherdata['forecast']['simpleforecast']['forecastday'][$i]['avehumidity'];
 		}
+
 		$weatherJson = json_encode($weatherArray);
-		file_put_contents($localWeatherData, $weatherJson);
+		file_put_contents($config['localWeatherData'], $weatherJson);
 		return $weatherJson;
 	}
 
