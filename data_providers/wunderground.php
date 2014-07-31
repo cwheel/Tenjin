6bd9fa0e8bbda903ad493e70869ec367ee23a791
sulsull <coolcmp424@yahoo.com>
2014-07-31 07:40:10 -0400

    Slight Optimization to Wunderground Plugin
    Changed how data is stored and removed some extraneous code.

--- data_providers/wunderground.php
+++ data_providers/wunderground.php
@@ -3,79 +3,61 @@
 	ini_set('display_errors', 1);
 	print_r(provideDataWunderground());
 	function provideDataWunderground() {
+
 		include('../config.php');
 	    $weatherArray = array();
+	    $loop = 0;
 
-		if (file_exists($localweatherDataHour)){
-			$downloadedweatherdata = checkFileDownload($localweatherDataHour, $weatherDataHour);
+		if (file_exists($localWeatherData)){
+			if (date("d",filemtime($localWeatherData)) != date("d")){
+				$downloadedweatherdata = downloadWeatherData($weatherDataHour);
+			}else if (date("H",filemtime($localWeatherData)) <= date("H") - 1){
+				$downloadedweatherdata = downloadWeatherData($weatherDataHour);
+			}else{
+				return file_get_contents($localWeatherData);
+			}
 		}else{
 			$downloadedweatherdata = downloadWeatherData($weatherDataHour);
 		}
 
-		$loopHour = 0; 
-		while ($loopHour < 36){
-			$weatherArray['hourly_forecast'][$loopHour]['hour'] = $downloadedweatherdata['hourly_forecast'][$loopHour]['FCTTIME']['hour'];
-			$weatherArray['hourly_forecast'][$loopHour]['temp'] = $downloadedweatherdata['hourly_forecast'][$loopHour]['temp'][$units];
-			$weatherArray['hourly_forecast'][$loopHour]['condition'] = $downloadedweatherdata['hourly_forecast'][$loopHour]['wx'];
-			$weatherArray['hourly_forecast'][$loopHour]['icon'] = $downloadedweatherdata['hourly_forecast'][$loopHour]['icon'];
-			$weatherArray['hourly_forecast'][$loopHour]['feelsLike'] = $downloadedweatherdata['hourly_forecast'][$loopHour]['feelslike'][$units];
-			$weatherArray['hourly_forecast'][$loopHour]['chancePrecipitation'] = $downloadedweatherdata['hourly_forecast'][$loopHour]['pop'];
-			$weatherArray['hourly_forecast'][$loopHour]['rainfall'] = $downloadedweatherdata['hourly_forecast'][$loopHour]['qpf'][$units];
-			$weatherArray['hourly_forecast'][$loopHour]['snowfall'] = $downloadedweatherdata['hourly_forecast'][$loopHour]['snow'][$units];
-			$weatherArray['hourly_forecast'][$loopHour]['windspeed'] = $downloadedweatherdata['hourly_forecast'][$loopHour]['wspd'][$units];
-
-			++$loopHour;
-		}
-		
-		if (file_exists($localweatherDataWeekly)){
-			$downloadedweatherdata = checkFileDownload($localweatherDataWeekly, $weatherDataWeekly);
-		}else{
-			$downloadedweatherdata = downloadWeatherData($weatherDataWeekly);
+		while ($loop < 36){
+			$weatherArray['hourly_forecast'][$loop]['hour'] = $downloadedweatherdata['hourly_forecast'][$loop]['FCTTIME']['hour'];
+			$weatherArray['hourly_forecast'][$loop]['temp'] = $downloadedweatherdata['hourly_forecast'][$loop]['temp'][$units];
+			$weatherArray['hourly_forecast'][$loop]['condition'] = $downloadedweatherdata['hourly_forecast'][$loop]['wx'];
+			$weatherArray['hourly_forecast'][$loop]['icon'] = $downloadedweatherdata['hourly_forecast'][$loop]['icon'];
+			$weatherArray['hourly_forecast'][$loop]['feelsLike'] = $downloadedweatherdata['hourly_forecast'][$loop]['feelslike'][$units];
+			$weatherArray['hourly_forecast'][$loop]['chancePrecipitation'] = $downloadedweatherdata['hourly_forecast'][$loop]['pop'];
+			$weatherArray['hourly_forecast'][$loop]['rainfall'] = $downloadedweatherdata['hourly_forecast'][$loop]['qpf'][$units];
+			$weatherArray['hourly_forecast'][$loop]['snowfall'] = $downloadedweatherdata['hourly_forecast'][$loop]['snow'][$units];
+			$weatherArray['hourly_forecast'][$loop]['windspeed'] = $downloadedweatherdata['hourly_forecast'][$loop]['wspd'][$units];
+			++$loop;
 		}
 
-		$loopWeekly = 0;
-		while ($loopWeekly < 7){
-			$weatherArray['weekly_forecast'][$loopWeekly]['date']['day'] = $downloadedweatherdata['forecast']['simpleforecast']['forecastday'][$loopWeekly]['date']['day'];
-			$weatherArray['weekly_forecast'][$loopWeekly]['date']['month'] = $downloadedweatherdata['forecast']['simpleforecast']['forecastday'][$loopWeekly]['date']['month'];
-			$weatherArray['weekly_forecast'][$loopWeekly]['date']['year'] = $downloadedweatherdata['forecast']['simpleforecast']['forecastday'][$loopWeekly]['date']['year'];
-			$weatherArray['weekly_forecast'][$loopWeekly]['date']['weekday'] = $downloadedweatherdata['forecast']['simpleforecast']['forecastday'][$loopWeekly]['date']['weekday'];
-			$weatherArray['weekly_forecast'][$loopWeekly]['temperature']['high'] = $downloadedweatherdata['forecast']['simpleforecast']['forecastday'][$loopWeekly]['high'][$tempunits];
-			$weatherArray['weekly_forecast'][$loopWeekly]['temperature']['low'] = $downloadedweatherdata['forecast']['simpleforecast']['forecastday'][$loopWeekly]['low'][$tempunits];
-			$weatherArray['weekly_forecast'][$loopWeekly]['chancePrecipitation'] = $downloadedweatherdata['forecast']['simpleforecast']['forecastday'][$loopWeekly]['pop'];
-			$weatherArray['weekly_forecast'][$loopWeekly]['rainfall'] = $downloadedweatherdata['forecast']['simpleforecast']['forecastday'][$loopWeekly]['qpf_day'][$volunits];
-			$weatherArray['weekly_forecast'][$loopWeekly]['snowfall'] = $downloadedweatherdata['forecast']['simpleforecast']['forecastday'][$loopWeekly]['snow_day'][$volunits];
-			$weatherArray['weekly_forecast'][$loopWeekly]['condition'] = $downloadedweatherdata['forecast']['simpleforecast']['forecastday'][$loopWeekly]['conditions'];
-			$weatherArray['weekly_forecast'][$loopWeekly]['icon'] = $downloadedweatherdata['forecast']['simpleforecast']['forecastday'][$loopWeekly]['icon'];
-			$weatherArray['weekly_forecast'][$loopWeekly]['windspeed'] = $downloadedweatherdata['forecast']['simpleforecast']['forecastday'][$loopWeekly]['avewind'][$speedunits];
-			$weatherArray['weekly_forecast'][$loopWeekly]['humidity'] = $downloadedweatherdata['forecast']['simpleforecast']['forecastday'][$loopWeekly]['avehumidity'];
-			++$loopWeekly;
+		$downloadedweatherdata = downloadWeatherData($weatherDataWeekly);
+		$loop = 0;
+		while ($loop < 7){
+			$weatherArray['weekly_forecast'][$loop]['date']['day'] = $downloadedweatherdata['forecast']['simpleforecast']['forecastday'][$loop]['date']['day'];
+			$weatherArray['weekly_forecast'][$loop]['date']['month'] = $downloadedweatherdata['forecast']['simpleforecast']['forecastday'][$loop]['date']['month'];
+			$weatherArray['weekly_forecast'][$loop]['date']['year'] = $downloadedweatherdata['forecast']['simpleforecast']['forecastday'][$loop]['date']['year'];
+			$weatherArray['weekly_forecast'][$loop]['date']['weekday'] = $downloadedweatherdata['forecast']['simpleforecast']['forecastday'][$loop]['date']['weekday'];
+			$weatherArray['weekly_forecast'][$loop]['temperature']['high'] = $downloadedweatherdata['forecast']['simpleforecast']['forecastday'][$loop]['high'][$tempunits];
+			$weatherArray['weekly_forecast'][$loop]['temperature']['low'] = $downloadedweatherdata['forecast']['simpleforecast']['forecastday'][$loop]['low'][$tempunits];
+			$weatherArray['weekly_forecast'][$loop]['chancePrecipitation'] = $downloadedweatherdata['forecast']['simpleforecast']['forecastday'][$loop]['pop'];
+			$weatherArray['weekly_forecast'][$loop]['rainfall'] = $downloadedweatherdata['forecast']['simpleforecast']['forecastday'][$loop]['qpf_day'][$volunits];
+			$weatherArray['weekly_forecast'][$loop]['snowfall'] = $downloadedweatherdata['forecast']['simpleforecast']['forecastday'][$loop]['snow_day'][$volunits];
+			$weatherArray['weekly_forecast'][$loop]['condition'] = $downloadedweatherdata['forecast']['simpleforecast']['forecastday'][$loop]['conditions'];
+			$weatherArray['weekly_forecast'][$loop]['icon'] = $downloadedweatherdata['forecast']['simpleforecast']['forecastday'][$loop]['icon'];
+			$weatherArray['weekly_forecast'][$loop]['windspeed'] = $downloadedweatherdata['forecast']['simpleforecast']['forecastday'][$loop]['avewind'][$speedunits];
+			$weatherArray['weekly_forecast'][$loop]['humidity'] = $downloadedweatherdata['forecast']['simpleforecast']['forecastday'][$loop]['avehumidity'];
+			++$loop;
 		}
-		return $weatherArray;
+		$weatherJson = json_encode($weatherArray);
+		file_put_contents($localWeatherData, $weatherJson);
+		return $weatherJson;
 	}
 
 	function downloadWeatherData($apiFile){
-
-				include('../config.php');
                 $weather = file_get_contents($apiFile);
-                if ( $apiFile == $weatherDataHour){
-                	file_put_contents($localweatherDataHour, $weather);
-                }else{
-                	file_put_contents($localweatherDataWeekly, $weather);                	
-                }
-
                 return json_decode($weather, TRUE);
         }
-
-    function checkFileDownload($filepath, $apiFile){
-    		// Downloads the JSON file if it is older than an hour, or doesn't exist.
-
-			if (date("d",filemtime($filepath) != date("d"))){
-				return downloadWeatherData($apiFile);
-			}else if (date("H",filemtime($filepath) <= date("H") - 1)){
-				return downloadWeatherData($apiFile);
-			}else{
-				file_get_contents($filepath, $weatherJson);
-				return json_decode($weatherJson, TRUE);
-			}
-		}
 ?>
