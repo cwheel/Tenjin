aa7a141d6ae66b7105533a07f2d0c8e8491bcf25
nosedive25 <nosedive25@git.git>
2014-08-04 13:32:41 -0400

    Started Pagination

--- js/pagination.js
+++ js/pagination.js
@@ -1,11 +1,13 @@
 $(document).ready(function () {
-	$("pg2").hide();
+	$("#pg2").css("left", "800px");
 });
 
 function showNext() {
-	if ($("pg1").css('left') == "50px") {
-
-	} else if ($("pg2").css('left') == "50px") {
-
+	if ($("#pg1").css('left') == "50px") {
+		$("#pg1").css("left", "800px");
+		$("#pg2").css("left", "50px");
+	} else if ($("#pg2").css('left') == "50px") {
+		$("#pg2").css("left", "800px");
+		$("#pg1").css("left", "50px");
 	}
 }
